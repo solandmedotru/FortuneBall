a39ff1ef86d375a93acc95eaf916691b565e219c
Andrey Solomin <solomin.andrey@gmail.com>
2016-08-27 17:03:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/solandme/fortuneball/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.solandme.fortuneball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
